0ecd8f95ddb0b202e8d72e9b44ba2b4ed43ede98
Kemmachat <kemmachat@Kemmachats-MacBook-Air.local>
2018-09-06 12:46:58 +0700

    Add Rental Rental-list Rental-Detail Rental Service Rental model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { RentalModule } from './rental/rental.module';
+ const routes: Routes = [
+  {path: '', redirectTo: '/rentals', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -11,7 +16,9 @@ import { HeaderComponent } from './common/header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
